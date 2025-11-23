a7a2426c023596479ad81e010e89dc264204f2e0
Deivid-Dev1 <davidmrquez3@gmail.com>
2025-11-23 12:13:36 +0100

    v3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.material.v1130)
 }
